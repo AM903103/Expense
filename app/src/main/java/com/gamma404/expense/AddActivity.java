65dd5afe0209cbea156b73db9f35bdbfa7dccfc4
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-07 07:28:05 +0800

    Add and layout AddActivity. Add Intent from MainActivity to Add Activity within fab button.

---
+++ app/src/main/java/com/gamma404/expense/AddActivity.java
@@ -0,0 +1,40 @@
+package com.gamma404.expense;
+
+import android.opengl.ETC1;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddActivity extends AppCompatActivity {
+
+    private static final String TAG = AddActivity.class.getSimpleName();
+    private EditText edDate;
+    private EditText edInfo;
+    private EditText edAmount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        findViews();
+    }
+
+    private void findViews() {
+        edDate = findViewById(R.id.add_ed_date);
+        edInfo = findViewById(R.id.add_ed_info);
+        edAmount = findViewById(R.id.add_ed_amount);
+    }
+
+    public void add(View view) {
+        String date = edDate.getText().toString();
+        String info = edInfo.getText().toString();
+        String amount = edAmount.getText().toString();
+        String result ="date :" + date + " ,info :" + info + " ,amount :" + amount;
+        Log.d(TAG, "add: " + result );
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+    }
+}
