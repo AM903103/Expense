2530cfe5ee5eb1f7a014e1732b43912b6145111f
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-07 08:17:12 +0800

    Add RecyclerView to show data in MainActivity.

--- app/src/main/java/com/gamma404/expense/MainActivity.java
+++ app/src/main/java/com/gamma404/expense/MainActivity.java
@@ -1,14 +1,20 @@
 package com.gamma404.expense;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +29,24 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
                 startActivity(intent);
             }
         });
+
+        setupRecycleView();
+    }
+
+    private void setupRecycleView() {
+        RecyclerView recyclerView =findViewById(R.id.main_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Cursor cursor = new DBHelper(this, "expense.db", null, 1)
+                .getReadableDatabase()
+                .query("exp", null, null, null, null, null, null);
+        ExpenseAdapter adapter = new ExpenseAdapter(cursor);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -51,5 +69,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private static class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHolder> {
+        private Cursor cursor;
+
+        public ExpenseAdapter(Cursor cursor) {
+            this.cursor = cursor;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_2, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            cursor.moveToPosition(position);
+            String cdate = cursor.getString(cursor.getColumnIndex("cdate"));
+            String info = cursor.getString((cursor.getColumnIndex("info")));
+            holder.dateTextView.setText(cdate);
+            holder.intoTextView.setText(info);
+        }
+
+        @Override
+        public int getItemCount() {
+            return cursor.getCount();
+        }
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+
+            private final TextView intoTextView;
+            private final TextView dateTextView;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                dateTextView = itemView.findViewById(android.R.id.text1);
+                intoTextView = itemView.findViewById(android.R.id.text2);
+            }
+        }
     }
 }
