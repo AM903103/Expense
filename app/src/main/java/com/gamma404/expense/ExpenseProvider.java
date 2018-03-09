891630a9ecc375613adb2bb2298a2069b86d6918
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-09 20:09:49 +0800

    Provider design

---
+++ app/src/main/java/com/gamma404/expense/ExpenseProvider.java
@@ -0,0 +1,52 @@
+package com.gamma404.expense;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by forev on 2018/3/9.
+ */
+
+public class ExpenseProvider extends ContentProvider {
+
+    public static final String TAG = ExpenseProvider.class.getSimpleName();
+
+    @Override
+    public boolean onCreate() {
+        Log.d(TAG, "onCreate: ");
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
