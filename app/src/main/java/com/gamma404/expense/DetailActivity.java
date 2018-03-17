1bb99ddc3108ffed807f553993ce94cecd5c2514
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-17 20:24:47 +0800

    DetailActivity implementation

--- app/src/main/java/com/gamma404/expense/DetailActivity.java
+++ app/src/main/java/com/gamma404/expense/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.gamma404.expense;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Expense expense = getIntent().getParcelableExtra("EXPENSE");
+        ((TextView)findViewById(R.id.detail_tv_date)).setText(expense.getDate());
+        ((TextView)findViewById(R.id.detail_tv_info)).setText(expense.getInfo());
+        ((TextView)findViewById(R.id.detail_tv_amount)).setText(expense.getAmount()+"");
     }
 }
