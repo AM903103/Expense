712ec08314880c1fd47011ba01a7a351a8965ab7
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-17 20:44:05 +0800

    Add Remove menuitem

--- app/src/main/java/com/gamma404/expense/DetailActivity.java
+++ app/src/main/java/com/gamma404/expense/DetailActivity.java
@@ -3,17 +3,44 @@ package com.gamma404.expense;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = DetailActivity.class.getSimpleName();
+    private Expense expense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Expense expense = getIntent().getParcelableExtra("EXPENSE");
-        ((TextView)findViewById(R.id.detail_tv_date)).setText(expense.getDate());
-        ((TextView)findViewById(R.id.detail_tv_info)).setText(expense.getInfo());
-        ((TextView)findViewById(R.id.detail_tv_amount)).setText(expense.getAmount()+"");
+        expense = getIntent().getParcelableExtra("EXPENSE");
+        ((TextView) findViewById(R.id.detail_tv_date)).setText(expense.getDate());
+        ((TextView) findViewById(R.id.detail_tv_info)).setText(expense.getInfo());
+        ((TextView) findViewById(R.id.detail_tv_amount)).setText(expense.getAmount() + "");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return true :menu to be displayed;
+        //return false: it will not be shown.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete) {
+            Log.d(TAG, "onOptionsItemSelected: ");
+            getContentResolver().delete(ExpenseContact.uri, " = ? ",
+                    new String[]{expense.getId() + ""});
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
